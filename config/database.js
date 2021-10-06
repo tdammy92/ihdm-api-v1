6295d2e932870264e12a929d07b8431ca68fea74
mercatorng <staywokewithmercator@gmail.com>
2021-10-06 09:28:02 +0100

    first deployement

---
+++ config/database.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        client: 'mongo',
+      host: env('DATABASE_HOST', 'bilek.f0ej3.mongodb.net'),
+       srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'ihdmDB'),
+        username: env('DATABASE_USERNAME', 'ihdm'),
+        password: env('DATABASE_PASSWORD', 'wanted'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE',null),
+        ssl: env('DATABASE_SSL',true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '65050595-b272-49ef-af8d-b3619c4020ac'
+};
