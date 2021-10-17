2ae952e46d7366271a5a12c677963807b2c0904c
mercatorng <staywokewithmercator@gmail.com>
2021-10-17 09:49:56 +0100

    added extra form details

--- config/database.js
+++ config/database.js
@@ -1,3 +1,24 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         client: 'mongo',
+//       host: env('DATABASE_HOST', 'bilek.f0ej3.mongodb.net'),
+//        srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'ihdmDB'),
+//         username: env('DATABASE_USERNAME', 'ihdm'),
+//         password: env('DATABASE_PASSWORD', 'wanted'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE',null),
+//         ssl: env('DATABASE_SSL',true),
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -5,16 +26,16 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         client: 'mongo',
-      host: env('DATABASE_HOST', 'bilek.f0ej3.mongodb.net'),
-       srv: env.bool('DATABASE_SRV', true),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+       srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'ihdmDB'),
-        username: env('DATABASE_USERNAME', 'ihdm'),
-        password: env('DATABASE_PASSWORD', 'wanted'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE',null),
-        ssl: env('DATABASE_SSL',true),
+        ssl: env('DATABASE_SSL',false),
       },
     },
   },
